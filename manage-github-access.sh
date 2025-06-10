a3e3e95f833a675f7f0d013cd6fb552876a0b6ec
naveenchelo <chelonaveen07@gmail.com>
2025-06-10 19:16:43 +0000

    fix:syntax issues in code

--- manage-github-access.sh
+++ manage-github-access.sh
@@ -7,6 +7,7 @@ usage(){
 	echo " $0 my-org my-repo"
 	echo " $0 my-org my-repo add alice write"
 	exit 1
+}
 
 # Check arguments
 if [ "$#" -lt 2 ]; then 
@@ -19,7 +20,7 @@ REPO="$2"
 # List collaborators
 list_collaborators() {
 	echo "Collaborators for $OWNER/$REPO:"
-	gh api/$OWNER/$REPO/collaborators | jq -r '(["Username","Type","Permission"], ["________","----","________"] ),(.[] | [.login, .type, .permissions | to_entries[] | select(.values == true) | .key])
+	gh api repos/$OWNER/$REPO/collaborators | jq -r '(["Username","Type","Permission"], ["________","----","________"] ),(.[] | [.login, .type, .permissions | to_entries[] | select(.value == true) | .key])
 	| @tsv' | \ 
         column -t
 }
@@ -30,12 +31,11 @@ add_collaborator() {
 	USER="$1"
 	PERM="${2:-push}" #Default to write/push permissions
 	echo "Adding user '$USER' to $OWNER/$REPO with '$PERM' permission..."
-	gh api repos/$OWNER/$REPO/collaborators/$USER --method PUT -f
-permission="$PERM"
+	gh api repos/$OWNER/$REPO/collaborators/$USER --method PUT -f permission="$PERM"
 
 #Main Logic
 
-if [ "$3 = "add" ] && [ -n "$4" ]; then
+if [ "$3" = "add" ] && [ -n "$4" ]; then
 	add_collaborator "$4" "$5"
 elif [ "$#" -eq 2 ]; then 
 	list_collaborators
